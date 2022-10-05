94a66791e2db5526974d302d4b2ba021255472ab
Luan de Castro Ribeiro <luan.ribeiro@estudante.ifgoiano.edu.br>
2022-10-05 19:23:28 -0300

    Criação de Interface

--- Banco-DIO-main/Banco DIO/src/Entidades/Banco.java
+++ Banco-DIO-main/Banco DIO/src/Entidades/Banco.java
@@ -16,16 +16,43 @@ public class Banco {
     public static List<ContaCorrente> contaCorrentes = new ArrayList<>();
     public static List<ContaPoupanca> contaPoupancas = new ArrayList<>();
 
-    public static void extratoCC() {
-        for(ContaCorrente cc : contaCorrentes) {
-            JOptionPane.showMessageDialog(null, cc.toString(),NOME_BANCO, JOptionPane.PLAIN_MESSAGE);
+    public static ContaCorrente getContaPorNumeroCC(String numConta){
+        for(ContaCorrente conta: contaCorrentes){
+            if(String.valueOf(conta.getNumeroConta()).equals( numConta))
+                return conta;
         }
+        return null;
     }
 
-    public static void extratoCP() {
-        for(ContaPoupanca cp : contaPoupancas) {
-            JOptionPane.showMessageDialog(null, cp.toString(),NOME_BANCO, JOptionPane.PLAIN_MESSAGE);
+    public static ContaPoupanca getContaPorNumeroCP(String numConta){
+        for(ContaPoupanca conta: contaPoupancas){
+            if(String.valueOf(conta.getNumeroConta()).equals( numConta))
+                return conta;
         }
+        return null;
+    }
+
+
+    public static String[][] extratoContas() {
+
+        String[][] dadosExtratos = new String[5][contaCorrentes.size() + contaPoupancas.size()];
+        int c;
+
+        for(c = 0; c < contaCorrentes.size(); c++) {
+            dadosExtratos[c][0] = String.valueOf(contaCorrentes.get(c).numeroConta);
+            dadosExtratos[c][1] = String.valueOf(contaCorrentes.get(c).agencia);
+            dadosExtratos[c][2] = String.valueOf(contaCorrentes.get(c).saldo);
+            dadosExtratos[c][3] = contaCorrentes.get(c).cliente.getCpf();
+            dadosExtratos[c][4] = "CC";
+        }
+        while(c < contaPoupancas.size()) {
+            dadosExtratos[c][1] = String.valueOf(contaPoupancas.get(c).agencia);
+            dadosExtratos[c][2] = String.valueOf(contaPoupancas.get(c).saldo);
+            dadosExtratos[c][3] = contaPoupancas.get(c).cliente.getCpf();
+            dadosExtratos[c][4] = "CP";
+            c++;
+        }
+        return dadosExtratos;
     }
 
 }
